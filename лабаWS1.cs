b4f1c7c427d67e4e8b8fa26f4efdba6d963d5868
Вяселева Динара <118040854+VyaselevaDinara@users.noreply.github.com>
2024-04-09 08:18:52 +0300

    Add files via upload

---
+++ лабаWS1.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    static double Factorial(double num)
+    {
+        double facty = 1;
+        for (double j = 1; j <= num; j++)
+        {
+            facty *= j;
+        }
+        return facty;
+    }
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите значение числа n");
+        double N = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите значение числа x");
+        double X = Convert.ToDouble(Console.ReadLine());
+        double result = 0;
+        for (double i = 0; i <= N; i++)
+        {
+            result += Math.Pow(-1, i) * Math.Pow(X, i) / Factorial(i);
+        }
+        Console.WriteLine(result);
+    }
+}
